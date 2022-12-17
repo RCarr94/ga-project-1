1eb33767117154beb19e8f4cea24690488d3a64e
Ryan Carr <ryan.g.carr@hotmail.com>
2022-12-17 02:46:04 -0500

    organize code for wincheck tomorrow

--- js/main.js
+++ js/main.js
@@ -1,103 +1,31 @@
-const players = {
-  null: "white",
-  1: "red",
-  "-1": "green",
-};
+// Variables
+const p1 = "red";
+const p2 = "green";
 
-const playerNames = {
-  1: "Player 1",
-  "-1": "Player 2",
-};
+let turn = 1;
 
 let score = {
   1: 0,
   "-1": 0,
 };
 
-// cached elements
+// Cached elements
 let tr = document.getElementsByTagName("tr");
 let tc = document.getElementsByTagName("td");
 let ts = document.querySelector(".box");
 let updates = document.querySelector("#title");
-const reset = document.querySelector("#reset-board");
 
-// win conditions
-
-const winCons = [
-  [35, 36, 37, 38],
-  [35, 29, 23, 17],
-  [35, 28, 21, 14],
-  [36, 37, 38, 39],
-  [36, 29, 22, 15],
-  [36, 30, 24, 18],
-  [37, 38, 39, 40],
-  [37, 30, 23, 16],
-  [37, 31, 25, 19],
-  [38, 39, 40, 41],
-  [38, 31, 24, 17],
-  [38, 30, 22, 14],
-  [38, 32, 26, 20],
-  [39, 31, 23, 15],
-  [39, 32, 25, 18],
-  [40, 32, 24, 16],
-  [40, 33, 26, 19],
-  [41, 33, 25, 17],
-  [41, 34, 27, 20],
-  [28, 21, 14, 7],
-  [28, 22, 16, 10],
-  [28, 29, 30, 31],
-  [29, 22, 15, 8],
-  [29, 23, 17, 11],
-  [29, 30, 31, 32],
-  [30, 23, 16, 9],
-  [29, 24, 18, 12],
-  [30, 31, 32, 33],
-  [31, 23, 15, 7],
-  [31, 24, 17, 10],
-  [31, 25, 19, 13],
-  [31, 32, 33, 34],
-  [32, 24, 16, 8],
-  [32, 25, 18, 11],
-  [33, 25, 17, 9],
-  [33, 26, 19, 12],
-  [34, 26, 18, 10],
-  [34, 27, 20, 13],
-  [21, 14, 7, 0],
-  [21, 15, 9, 3],
-  [21, 22, 23, 24],
-  [22, 15, 8, 1],
-  [22, 16, 10, 4],
-  [22, 23, 24, 25],
-  [23, 16, 9, 2],
-  [23, 17, 11, 5],
-  [23, 24, 25, 26],
-  [24, 16, 7, 0],
-  [24, 17, 10, 3],
-  [24, 18, 12, 6],
-  [24, 25, 26, 27],
-  [25, 17, 9, 1],
-  [25, 18, 11, 4],
-  [26, 18, 10, 2],
-  [26, 19, 12, 5],
-  [27, 19, 11, 3],
-  [27, 20, 13, 6],
-  [14, 15, 16, 17],
-  [15, 16, 17, 18],
-  [16, 17, 18, 19],
-  [17, 18, 19, 20],
-  [7, 8, 9, 10],
-  [8, 9, 10, 11],
-  [9, 10, 11, 12],
-  [10, 11, 12, 13],
-  [0, 1, 2, 3],
-  [1, 2, 3, 4],
-  [2, 3, 4, 5],
-  [3, 4, 5, 6],
-];
+// Event listeners
+// 1. confirm if scores will remain at top (once completed)
+document.querySelector("#reset-board").addEventListener("click", function () {
+  initialize();
+  render();
+  updates.innerHTML = "Let's play again! Press 'Start Game' to see who goes first!";
+});
 
 // Stat tracking variables
 
-let board, turn, winner;
+let board, winner;
 
 // load initial state of game
 function initialize() {
@@ -191,17 +119,6 @@ function render() {
     updates.innerHTML = `${playerNames[turn]}, your move.`;
   }
 }
-
-// disable rows that dont have null zones below them
-// if (background-color !== red || green) {}
-
-// reset board
-// 1. confirm if scores will remain at top (once completed)
-document.querySelector("#reset-board").addEventListener("click", function () {
-  initialize();
-  render();
-  updates.innerHTML = "Let's play again!";
-});
 
 // reset score - check tic-tac-toe reset
 
